871e7be2272399dbb7d63a1ee669731b3af00169
Vaidheeswaran Srinivasan <vsrinivasan@presidio.com>
2025-11-11 05:49:18 +0530

    feat: created the user and product schema

---
+++ backend/src/models/product.js
@@ -0,0 +1,62 @@
+﻿import mongoose from "mongoose";
+import validator from "validator";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minLenghth: 3,
+      maxLength: 100,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 500,
+      maxLength: 1000,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: ["Men", "Women", "Kids"],
+    },
+    subCategory: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 3,
+      maxLength: 50,
+    },
+    sizes: {
+      type: [String],
+      required: true,
+      validate: {
+        validator: function (v) {
+          return v.length > 0;
+        },
+        message: "At least one size must be specified.",
+      },
+    },
+    date: {
+      type: Number,
+      requyired: true,
+    },
+    bestSeller: {
+      type: Boolean,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
---
+++ backend/src/models/user.js
@@ -0,0 +1,59 @@
+﻿import mongoose, { mongo } from "mongoose";
+import validator, { trim } from "validator";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 3,
+      maxLength: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate: {
+        validator: function (v) {
+          return validator.isEmail(v);
+        },
+        message: "Invalid email format.",
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      validate: {
+        validator: function (v) {
+          return validator.isStrongPassword(v, {
+            minLength: 8,
+            minLowercase: 1,
+            minUppercase: 1,
+            minNumbers: 1,
+            minSymbols: 1,
+          });
+        },
+        message:
+          "Password must be at least 8 characters long and include at least one lowercase letter, one uppercase letter, one number, and one symbol.",
+      },
+    },
+    cartData: {
+      type: Object,
+      default: {},
+    },
+  },
+  {
+    timestamps: true,
+  },
+  {
+    minimize: false, // this ensures empty objects are saved. Initially cartData is an empty object when new user is created
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
